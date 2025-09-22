d85aae72feac60e88c114a771da47bb6bd49d90f
Ow3nR4H <raulowen98@gmail.com>
2025-09-22 11:23:12 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Instalado");
+  e.waitUntil(
+    caches.open("pupuseria-v1").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/bebidas.html",
+        "/pupuseria.js",
+        "/favicon.png",
+        "/offline.html",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
